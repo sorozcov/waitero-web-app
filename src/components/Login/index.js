694dc832fba9a33438f74e919192243359355446
sorozcov <42949349+sorozcov@users.noreply.github.com>
2021-03-23 01:24:16 -0600

    Login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -27,26 +27,29 @@ function LoginPage() {
       />
       <form className="mt-8 space-y-6" action="#" method="POST">
       <input type="hidden" name="remember" value="true"/>
+      
       <div className="rounded-md shadow-sm -space-y-px">
         <div>
-          <label htmlFor="email-address" className="sr-only">Usuario</label>
-          <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:z-10 sm:text-sm" placeholder="Usuario"/>
+        
+          <label htmlFor="email-address" className="sr-only"> Usuario</label>
+          
+          <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Usuario"/>
         </div>
         <div>
           <label htmlFor="password" className="sr-only">Contraseña</label>
-          <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:z-10 sm:text-sm" placeholder="Contraseña"/>
+          <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Contraseña"/>
         </div>
       </div>
 
       <div className="flex items-center justify-between">
         <div className="text-sm">
-          <a href="#" className="font-small text-primary-600 hover:text-primary-500">
+          <a href="#" className="font-small text-indigo-600 hover:text-indigo-500">
             Si olvidaste tu contraseña, haz click aquí.
           </a>
         </div>
       </div>
       <div>
-        <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+        <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
          
           Iniciar Sesión
         </button>
