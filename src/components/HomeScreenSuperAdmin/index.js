8c862c7ed8f7bc02b30844316997fefaa79085db
Luis Pedro Cuéllar <lpcuellar14@gmail.com>
2021-03-24 05:57:43 -0600

    added restaurant link to navbar

--- src/components/HomeScreenSuperAdmin/index.js
+++ src/components/HomeScreenSuperAdmin/index.js
@@ -2,11 +2,11 @@ import logo from '../../assets/waitero.svg';
 import './index.css';
 import { withRouter } from "react-router-dom";
 import { history } from '../App/App.js';
+import { useHistory } from 'react-router-dom';
 
 function HomeScreenSuperAdmin() {
+  const history = useHistory();
   return (
-
- 
     <div>
       <nav className="bg-primary">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -19,7 +19,7 @@ function HomeScreenSuperAdmin() {
                 <div className="ml-5 flex items-baseline space-x-4">
                   {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                   <button className="bg-secondary text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</button>
-                  <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Restaurantes</button>
+                  <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" onClick = { () => history.push('/restaurants')}>Restaurantes</button>
                   <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Usuarios</button>
                   <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Clientes</button>
                   <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Reportes</button>
