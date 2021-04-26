64800336a1e20d5c62b375d06d2679152a326291
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2021-04-26 01:27:51 -0600

    Tabla de usuario

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,9 +1,12 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { connect } from 'react-redux';
 
 import './styles.css';
+import * as selectors from '../../logic/reducers';
+import * as actionsUsers from '../../logic/actions/users';
 
-const Users = () => {
-
+const Users = ({ users, fetchUsers }) => {
+    useEffect(fetchUsers,[fetchUsers]);
     return (
         <Fragment>
             <header className="bg-white shadow">
@@ -15,6 +18,11 @@ const Users = () => {
             </header>
             <main>
                 <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                    <div className="flex justify-end">
+                        <button className="bg-transparent hover:bg-blue-500 mb-5 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                            Nuevo Usuario
+                        </button>
+                    </div>
                     <div className="flex justify-center">
                         <div className="w-full">
                             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -22,16 +30,16 @@ const Users = () => {
                                     <thead className="bg-gray-50">
                                         <tr>
                                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Nombre
+                                            Usuario
                                         </th>
                                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Correo
+                                            Nombre Completo
                                         </th>
                                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             Tipo
                                         </th>
                                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Restaurante
+                                            Número de teléfono
                                         </th>
                                         <th scope="col" className="relative px-6 py-3">
                                             <span className="sr-only">Edit</span>
@@ -39,38 +47,37 @@ const Users = () => {
                                         </tr>
                                     </thead>
                                     <tbody className="bg-white divide-y divide-gray-200">
-                                        <tr>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <div className="flex items-center">
-                                            <div className="flex-shrink-0 h-10 w-10">
-                                                <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="" />
-                                            </div>
-                                            <div className="ml-4">
-                                                <div className="text-sm font-medium text-gray-900">
-                                                Jane Cooper
+                                        {users.map(user => (
+                                        <tr key={user.id}>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <div className="flex items-center">
+                                                <div className="flex-shrink-0 h-10 w-10">
+                                                    <img className="h-8 w-8 rounded-full" src={`https://avatar.oxro.io/avatar.svg?name=${user == null ? "" : user.username}&background=F9DC5C&color=000`} alt="" />
                                                 </div>
-                                                <div className="text-sm text-gray-500">
-                                                jane.cooper@example.com
+                                                <div className="ml-4">
+                                                    <div className="text-sm font-medium text-gray-900">
+                                                        {user.username}
+                                                    </div>
+                                                    <div className="text-sm text-gray-500">
+                                                        {user.email}
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <div className="text-sm text-gray-900">Regional Paradigm Technician</div>
-                                            <div className="text-sm text-gray-500">Optimization</div>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                            Active
-                                            </span>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            Admin
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                        </td>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                                {user.first_name} {user.last_name}
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                                {user.userType_display}
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                                {user.phoneNumber}
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a href="#" className="text-indigo-600 hover:text-indigo-900">Editar</a>
+                                            </td>
                                         </tr>
+                                        ))}
                                     </tbody>
                                 </table>
                             </div>
@@ -82,4 +89,14 @@ const Users = () => {
     );
 };
 
-export default Users;
+export default connect(
+    state => ({
+      isLoading: selectors.getIsAuthenticating(state),
+      users: selectors.getUsers(state),   
+    }),
+    dispatch => ({
+      fetchUsers(values) {
+        dispatch(actionsUsers.startFetchingUsers(values));
+      },
+    }),
+  )(Users);
