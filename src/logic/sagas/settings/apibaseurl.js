c7aa4f53e10b7ac60095883ef47059b049405bc6
sorozcov <42949349+sorozcov@users.noreply.github.com>
2021-05-23 11:21:44 -0600

    API BASE URL

--- src/logic/sagas/settings/apibaseurl.js
+++ src/logic/sagas/settings/apibaseurl.js
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------------- */
 /*                                 APIBASEURL                                 */
 /* -------------------------------------------------------------------------- */
-const API_BASE_URL = `http://ec2-18-220-90-146.us-east-2.compute.amazonaws.com:80/api/v1`;
+const API_BASE_URL = `http://ec2-3-142-76-26.us-east-2.compute.amazonaws.com:80/api/v1`;
 //const API_BASE_URL = `http://localhost:8000/api/v1`;
 export default API_BASE_URL;
